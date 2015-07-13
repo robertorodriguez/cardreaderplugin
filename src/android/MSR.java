61390cfcf1602c6648347e882847cf2b73341d07
roberto <roberto@debian>
2015-07-13 18:18:30 -0300

    fix

--- src/android/MSR.java
+++ src/android/MSR.java
@@ -39,6 +39,9 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
+import java.io.UnsupportedEncodingException;
+
+
 
 import android.util.Log;
 import android.util.TimeUtils;
